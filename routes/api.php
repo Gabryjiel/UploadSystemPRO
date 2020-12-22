6c5ab77f766609dc1bd7f6a0a8978b9eee716dbc
Gabryjiel <33352685+Gabryjiel@users.noreply.github.com>
2020-12-22 19:05:32 +0100

    Remove useless resources, add test routes

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('test', function () {
+    return [
+        'test' => 'test',
+        'array' => [1,2,3]
+    ];
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// TODO: Entry point to React App, probably breaks with react router
+
+Route::get('/{path?}', function () {
+    return File::get(resource_path('react/public/index.html'));
+})->where('path', '^((?!api).)*$');
