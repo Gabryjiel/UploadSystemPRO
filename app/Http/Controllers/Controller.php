35bc0128706653425e5154e5d29dab7c1d9eac1e
Gabryjiel <33352685+Gabryjiel@users.noreply.github.com>
2020-12-28 23:01:35 +0100

    basic session authentication with cookies

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,19 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected function currentUser() {
+        return auth()->user();
+    }
+
+    protected function userNotAuthorized() {
+        return response()->json([
+            'success' => false,
+            'message' => 'User not authorized'
+        ], 403);
+    }
+
+    protected function rolesRequired($roles) {
+        return in_array($this->currentUser->role, $roles);
+    }
 }
