491efadf4a2ee4ff96407ed770bee1009dba6ce9
daspharion <daspharion@gmail.com>
2021-01-02 14:37:26 +0200

    added: custom tailwind styles

--- resources/views/Landing.tsx
+++ resources/views/Landing.tsx
@@ -16,16 +16,16 @@ export const Landing = () => {
   }
 
   return (
-    <div className="flex flex-col">
-      <header className="flex flex-row justify-between px-5 h-15vh sm:h-10vh">
-        <div className="flex flex-row items-center space-x-2 text-lg md:text-2xl">
+    <div className="stack">
+      <header className="hstack justify-between px-5 h-15vh sm:h-10vh">
+        <div className="hstack items-center space-x-2 text-lg md:text-2xl">
           <img src={logo} width="32px" height="32px" />
-          <div className="font-bold text-center flex flex-col sm:flex-row sm:space-x-2">
+          <div className="stack font-bold text-center sm:flex-row sm:space-x-2">
             <h1>Upload System</h1>
             <h1>PRO</h1>
           </div>
         </div>
-        <div className="flex flex-row items-center text-lg space-x-5 md:space-x-8">
+        <div className="hstack items-center text-lg space-x-5 md:space-x-8">
           <a className="cursor-pointer" onClick={() => scrollTo(logInRef)}>Log In</a>
           <a className="cursor-pointer" onClick={() => scrollTo(signUpRef)}>Sign Up</a>
           <img
@@ -36,18 +36,18 @@ export const Landing = () => {
       </header>
 
       <main>
-        <section className="flex h-85vh sm:h-90vh items-center justify-center bg-red-400">
+        <section className="landing-tile h-85vh sm:h-90vh bg-red-400">
           <div className="text-center">
             <p>SOME SIMPLISTIC BUT RATHER COOL LOOKING PICTURE</p>
             <p>WITH A HUGE "LOG IN" BUTTON IN THE MIDDLE</p>
           </div>
         </section>
 
-        <section className="flex h-screen items-center justify-center bg-gray-400" ref={logInRef}>
+        <section className="landing-tile bg-gray-400" ref={logInRef}>
           <p>LOG IN FORM</p>
         </section>
 
-        <section className="flex h-screen items-center justify-center bg-blue-400" ref={signUpRef}>
+        <section className="landing-tile bg-blue-400" ref={signUpRef}>
           <p>SIGN UP FORM</p>
         </section>
       </main>
