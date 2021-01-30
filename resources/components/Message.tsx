adc178b388fcc04bfc98133ceb7217f1840c584c
daspharion <daspharion@gmail.com>
2021-01-30 18:25:18 +0200

    qol: extracted CloseIcon from the Message comopent

--- resources/components/Message.tsx
+++ resources/components/Message.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { IconClose } from '../icons'
 
 type Props = {
   ctx: TMessage;
@@ -21,12 +22,7 @@ export const Message = (props: Props) => {
   return ctx.text ? (
     <div className={`hstack justify-between items-center mt-2 text-sm p-2 px-4 border rounded ${getVariant(ctx)}`}>
       <span>{ctx.text}</span>
-      <svg
-        className='cursor-pointer' width='12px' height='12px' onClick={onClose}
-        fill='currentColor' strokeWidth='0' viewBox='0 0 16 16' xmlns='http://www.w3.org/2000/svg'
-      >
-        <path fillRule='evenodd' d='M3.72 3.72a.75.75 0 011.06 0L8 6.94l3.22-3.22a.75.75 0 111.06 1.06L9.06 8l3.22 3.22a.75.75 0 11-1.06 1.06L8 9.06l-3.22 3.22a.75.75 0 01-1.06-1.06L6.94 8 3.72 4.78a.75.75 0 010-1.06z' />
-      </svg>
+      <IconClose className='w-3 cursor-pointer' onClick={onClose} />
     </div>
   ) : null
 }
