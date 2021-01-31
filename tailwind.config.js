5f220d0b244cf3bd6d0f8124e4191329e57cbea5
daspharion <daspharion@gmail.com>
2021-01-31 02:06:06 +0200

    qol: moved over some of the styles to their react components

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,10 +14,13 @@ module.exports = {
         '108': '36rem'
       },
       boxShadow: {
-        white: '0 1px 3px 0 rgba(255, 255, 255, 0.1), 0 1px 2px 0 rgba(255, 255, 255, 0.06);',
+        white: '0 1px 3px 0 rgba(255, 255, 255, 0.1), 0 1px 2px 0 rgba(255, 255, 255, 0.06)',
       },
       minHeight: {
         '10': '10vh', '15': '15vh', '20': '20vh', '30': '30vh', '40': '40vh', '50': '50vh', '60': '60vh', '70': '70vh', '80': '80vh', '85': '85vh', '90': '90vh'
+      },
+      borderWidth: {
+        '1': '1px'
       }
     }
   },
@@ -25,7 +28,11 @@ module.exports = {
     extend: {
       display: ['dark'],
       boxShadow: ['dark'],
-      fontWeight: ['dark']
+      fontWeight: ['dark'],
+      opacity: ['disabled'],
+      cursor: ['disabled'],
+      textColor: ['disabled'],
+      backgroundColor: ['disabled']
     }
   },
   plugins: []
