c116231404c0c62bdf07dfcafb3bc2a76ecdf70e
daspharion <daspharion@gmail.com>
2021-02-03 20:24:18 +0200

    fixed: element highlight on nested routes

--- resources/views/Dashboard.tsx
+++ resources/views/Dashboard.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import { Navigation } from '../components/Navigation'
 import { Subjects } from '../components/Subjects'
+import { AddSubject } from '../components/AddSubject'
 
 export default function Landing () {
   return (
@@ -9,6 +10,7 @@ export default function Landing () {
       <Navigation />
       <main className='mx-3 mt-5 sm:mx-10'>
         <Switch>
+          <Route path='/classes/new'><AddSubject role={0} /></Route>
           <Route path='/classes'><Subjects role={0} /></Route>
         </Switch>
       </main>
