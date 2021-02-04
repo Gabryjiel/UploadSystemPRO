70021044222a45eb298f8656ef89cd3ff3e0ae67
daspharion <daspharion@gmail.com>
2021-02-04 02:34:59 +0200

    added: class creation view

---
+++ resources/components/AddSubject.tsx
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
+import { request } from '../utils'
+import { InputText } from './InputText'
+import { Message, TMessage } from './Message'
+import { Select } from './Select'
+import { Loader } from './Loader'
+
+
+type Props = {
+  role: number;
+}
+
+const groupNames = ['1EF-DI', '2EF-DI', '3EF-DI', '1XF-XD', '2KE-KW']
+
+export const AddSubject = (props: Props) => {
+  const { errors, register, handleSubmit, reset, formState } = useForm({ mode: 'onSubmit' })
+  const [groups, setGroups] = useState<string | null>(null)
+  const [feedback, setFeedback] = useState<TMessage>({ text: '' })
+
+  useEffect(() => {
+    // request<string[]>('groups').then(Object.values).then(setGroups)
+  }, [])
+
+  const onSubmit = (payload: Record<string, string>) => {
+    return request<string>('subjects', { method: 'post', body: JSON.stringify(payload) })
+      .then(() => {
+        reset({ name: '', group: null, subgroup: null, description: '' })
+        setFeedback({ variant: 'success', text: 'You have successfully created a new class!'} )
+      })
+      .catch(() => setFeedback({ variant: 'error', text: 'An error has occurred. Please try again later'} ))
+  }
+
+  const validateName = (input: string) => {
+    if (input === '') return 'Please enter a class name'
+    if (input.length < 3) return 'Class name is too short'
+    if (input.indexOf('\\') !== -1) return 'Class name contains a forbidden character!'
+  }
+
+  const validateGroup = (input: string) => {
+    if (input === '') return 'Please select a group'
+  }
+
+  const validateSubgroup = (input: string) => {
+    if (input === '') return 'Please select a subgroup'
+  }
+
+  const validateDescription = (input: string) => {
+    if (input === '') return 'Please provide a description for the class'
+    if (input.length < 3) return 'Description is too short'
+    if (input.indexOf('\\') !== -1) return 'Description contains a forbidden character!'
+  }
+
+  return (
+    <div className='stack'>
+      <div className='hstack mb-5 justify-between'>
+        <h1 className='text-2xl sm:text-3xl px-1 pb-2 mt-1 border-l-1 border-current select-none'>add new class</h1>
+        <Link className='self-center border-current border-1 px-3 py-1 cursor-pointer hover:text-white hover:bg-black dark:hover:text-black dark:hover:bg-gray-200' to='/classes'>return</Link>
+      </div>
+
+      {groups === null && <Loader />}
+
+      <form noValidate className={`grid gap-10 gap-y-5 grid-cols-2 w-full max-w-screen-lg mx-auto${groups === null ? ' opacity-50 pointer-events-none' : ''}`} onSubmit={handleSubmit(onSubmit)}>
+        <InputText
+          className='col-span-full' name='name' variant='underlined' label='class name' placeholder='class name'
+          required maxLength={64} ref={register({ validate: validateName })} error={errors?.name?.message}
+        />
+        <Select name='group' placeholder='group name' error={errors?.group?.message} ref={register({ validate: validateGroup })}>
+          {groupNames.map((v, i) => <option key={i} value={v}>{v}</option>)}
+        </Select>
+        <Select name='subgroup' placeholder='subgroup' error={errors?.subgroup?.message} ref={register({ validate: validateSubgroup })}>
+          {[...new Array(16)].map((v, i) => <option key={i} value={i+1}>L{i+1}</option>)}
+        </Select>
+        <InputText
+          className='col-span-full' name='description' variant='underlined' label='class description' placeholder='class description'
+          ref={register({ validate: validateDescription })} error={errors?.description?.message}
+        />
+        <Message ctx={feedback} className='col-span-full' onClose={() => setFeedback({ text: '' })} />
+        <input type='reset' value='reset' className={`col-auto mr-auto mt-2 px-10 border-current border-1 py-1 cursor-pointer bg-transparent \
+hover:text-white dark:hover:text-black focus:outline-none text-red-500 hover:bg-red-500`} />
+        <input type='submit' value='create' disabled={formState.isSubmitting} className={`col-auto ml-auto mt-2 px-10 border-current border-1 py-1 cursor-pointer bg-transparent \
+hover:text-white hover:bg-black dark:hover:text-black dark:hover:bg-gray-200 focus:outline-none disabled:cursor-wait disabled:opacity-20`} />
+      </form>
+    </div>
+  )
+}
--- resources/components/Subjects.tsx
+++ resources/components/Subjects.tsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect, Fragment } from 'react'
+import { Link } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
 import { request } from '../utils'
 import { Loader } from './Loader'
 import { InputText } from './InputText'
 import { TSubject } from '../typings'
+import { IconPlus } from '../icons'
 
 type Props = {
   role: number;
@@ -59,16 +61,15 @@ export const Subjects = (props: Props) => {
             className='text-md' ref={register()} onClose={() => reset({ search: '' })}
           />
          </div>
+        <Link className='hidden sm:block self-center border-current border-1 px-3 py-1 cursor-pointer hover:text-white hover:bg-black dark:hover:text-black dark:hover:bg-gray-200' to='/classes/new'>new</Link>
       </div>
 
       <SubjectTable />
       {subjects === null && <Loader />}
 
-      <button className='sm:hidden fixed bottom-20 right-5 p-0 w-12 h-12 bg-red-600 rounded-full hover:bg-red-700 active:shadow-lg mouse shadow transition ease-in duration-200 focus:outline-none'>
-        <svg className='w-6 h-6 inline-block' viewBox='0 0 20 20' enable-background='new 0 0 20 20'>
-          <path fill='white' d='M16,10c0,0.553-0.048,1-0.601,1H11v4.399C11,15.951,10.553,16,10,16c-0.553,0-1-0.049-1-0.601V11H4.601C4.049,11,4,10.553,4,10c0-0.553,0.049-1,0.601-1H9V4.601C9,4.048,9.447,4,10,4c0.553,0,1,0.048,1,0.601V9h4.399C15.952,9,16,9.447,16,10z' />
-        </svg>
-      </button>
+      <Link to='/classes/new' className='sm:hidden fixed bottom-20 flex justify-center right-5 w-12 h-12 bg-red-600 rounded-full hover:bg-red-700 active:shadow-lg shadow transition ease-in duration-200 focus:outline-none'>
+        <IconPlus className='w-6 h-6 self-center' />
+      </Link>
     </div>
   )
 }
